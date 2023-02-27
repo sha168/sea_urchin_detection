ac0f0e82e4800ba62fbaa27d7a10e4c302002f3c
stine <stine_hansen@outlook.com>
2023-02-27 16:28:11 +0100

    run

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import torch
 from pathlib import Path
 
 
-BATCH_SIZE = 10  # increase / decrease according to GPU memeory
+BATCH_SIZE = 8  # increase / decrease according to GPU memeory
 RESIZE_TO = 512  # resize the image for training and transforms
 NUM_EPOCHS = 100  # number of epochs to train for
 NUM_WORKERS = 4
