5faf7f71602bab051a4a3adb21138eddf2f7af5d
sha168 <stine_hansen@outlook.com>
2023-03-06 11:30:07 +0100

    run

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import numpy as np
 
 BATCH_SIZE = 10  # increase / decrease according to GPU memeory
 RESIZE_TO = 512  # 512  # resize the image for training and transforms
-NUM_EPOCHS = 100  # number of epochs to train for
+NUM_EPOCHS = 10  # number of epochs to train for
 NUM_WORKERS = 4
 LR = 0.001
 WEIGHT_DECAY = 0.0005
 MOMENTUM = 0.9
-DATASET = 'UIQS'  # 'AUDD' or 'UIQS'
+DATASET = 'AUDD'  # 'AUDD' or 'UIQS'
 
 # whether to visualize images after crearing the data loaders
 VISUALIZE_TRANSFORMED_IMAGES = False
@@ -19,8 +19,8 @@ Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-#TRAIN_DIR = '/experiment/' + DATASET + '/train'
-#VALID_DIR = '/experiment/' + DATASET + '/test'
+TRAIN_DIR = '/experiment/' + DATASET + '/train'
+VALID_DIR = '/experiment/' + DATASET + '/test'
 
 # TRAIN_DIR = '/Users/sha168/Downloads/' + DATASET + '/train'
 # VALID_DIR = '/Users/sha168/Downloads/' + DATASET + '/test'
@@ -40,7 +40,7 @@ COLORS = np.random.uniform(0, 255, size=(NUM_CLASSES, 3))
 
 # location to save model and plots
 SAVE_PLOTS_EPOCH = 2  # save loss plots after these many epochs
-SAVE_MODEL_EPOCH = 50  # save model after these many epochs
+SAVE_MODEL_EPOCH = 1  # save model after these many epochs
 
 # Testing video
 PRETRAINED = 'gdrive/MyDrive/sea_urchin_detection/uiqs_model.pth'
