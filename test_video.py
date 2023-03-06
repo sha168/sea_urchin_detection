78162328f1795d1f5c0a5e35ce6bc1f8a5fa76b1
sha168 <stine_hansen@outlook.com>
2023-03-06 09:53:25 +0100

    run

--- test_video.py
+++ test_video.py
@@ -54,14 +54,12 @@ def _infer_stream(path_to_input_stream_endpoint, path_to_output_stream_endpoint,
             masked_frame = image_resized
             for bbox, cls, prob in zip(detection_bboxes.tolist(), detection_classes.tolist(), detection_probs.tolist()):
                 if cls == 1:  # only interested in urchins
-                    color = (np.round(np.random.random(size=3) * 256).astype('int'))
                     bbox = BBox(left=bbox[0], top=bbox[1], right=bbox[2], bottom=bbox[3])
                     category = 'urchin'
 
-                    print()
-                    masked_frame = cv2.rectangle(masked_frame, (bbox.left, bbox.top), (bbox.right, bbox.bottom), color, 2)
+                    masked_frame = cv2.rectangle(masked_frame, (bbox.left, bbox.top), (bbox.right, bbox.bottom), (0,0,255), 2)
                     masked_frame = cv2.putText(
-                        masked_frame, (bbox.left, bbox.top), f'{category:s} {prob:.3f}', cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+                        masked_frame, (bbox.left, bbox.top), f'{category:s} {prob:.3f}', cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,255), 2
                     )
 
             # Check if the video writer is None
