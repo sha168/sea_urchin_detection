cc9f716b7c133f662dbdb1ed8af77d0e57d70ebd
sha168 <stine_hansen@outlook.com>
2023-03-06 09:44:08 +0100

    run

--- test_video.py
+++ test_video.py
@@ -6,7 +6,7 @@ import cv2
 from PIL import Image
 from model import create_model
 from torchvision.transforms import transforms
-
+import numpy as np
 from config import NUM_CLASSES, PRETRAINED, DEVICE, RESIZE_TO, PERIOD, PROB_THRES, VIDEO_IN, VIDEO_OUT
 from bbox import BBox
 
@@ -54,13 +54,14 @@ def _infer_stream(path_to_input_stream_endpoint, path_to_output_stream_endpoint,
             masked_frame = image_resized
             for bbox, cls, prob in zip(detection_bboxes.tolist(), detection_classes.tolist(), detection_probs.tolist()):
                 if cls == 1:  # only interested in urchins
-                    color = random.choice(['red', 'green', 'blue', 'yellow', 'purple', 'white'])
+                    color = list(np.random.random(size=3) * 256)
                     bbox = BBox(left=bbox[0], top=bbox[1], right=bbox[2], bottom=bbox[3])
                     category = 'urchin'
 
-                    masked_frame = cv2.rectangle(masked_frame, (bbox.left, bbox.top), (bbox.right, bbox.bottom), color, 2)
+                    print()
+                    masked_frame = cv2.rectangle(image=masked_frame, start_point=(bbox.left, bbox.top), end_point=(bbox.right, bbox.bottom), color=color, thickness=2)
                     masked_frame = cv2.putText(
-                        masked_frame, (bbox.left, bbox.top), f'{category:s} {prob:.3f}', cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+                        image=masked_frame, org=(bbox.left, bbox.top), text=f'{category:s} {prob:.3f}', font=cv2.FONT_HERSHEY_COMPLEX, font_scale=0.7, color=color, thickness=2
                     )
 
             # Check if the video writer is None
