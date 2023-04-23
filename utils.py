f40abf1cfcc862e93f306d23ea5941be35e725f9
sha168 <stine_hansen@outlook.com>
2023-04-23 21:08:49 +0200

    run

--- utils.py
+++ utils.py
@@ -320,7 +320,7 @@ def evaluate(model, data_loader, device):
 
     # accumulate predictions from all images
     coco_evaluator.accumulate()
-    coco_evaluator.summarize()
+    #coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
     return coco_evaluator
 
