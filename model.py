48eb23d0184bf0af5debfd3423bca4cfc152a2b2
sha168 <stine_hansen@outlook.com>
2023-04-23 19:05:58 +0200

    run

--- model.py
+++ model.py
@@ -4,20 +4,19 @@ import torch
 import torch.nn as nn
 
 def create_model(num_classes, pretrained=None):
-    print('test!!!!!!!!!!!!!!!!', flush=True)
     # load Faster RCNN pre-trained model
     if pretrained == None:
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights='DEFAULT')
     else:
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
-    print('test!!!!!!!!!!!!!!!!', flush=True)
+
     # get the number of input features
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # define a new head for the detector with required number of classes
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    print('test!!!!!!!!!!!!!!!!', flush=True)
+    print(num_classes, flush=True)
+    raise
     if pretrained != None:
         model.load_state_dict(torch.load(pretrained, map_location="cpu"))
-    print('test!!!!!!!!!!!!!!!!', flush=True)
+
     return model
