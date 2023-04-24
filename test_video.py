85d3d5fbef60ef6ed80e3242bbf829cdcc77a8b7
sha168 <stine_hansen@outlook.com>
2023-04-24 11:21:22 +0200

    run

--- test_video.py
+++ test_video.py
@@ -18,7 +18,7 @@ def test_video(path_to_input_stream_endpoint, path_to_output_stream_endpoint, pr
     # Initialize the video stream and pointer to output video file
     vs = cv2.VideoCapture(path_to_input_stream_endpoint)
     # vs.set(cv2.CAP_PROP_POS_FRAMES, 7000)
-    fps = vs.get(cv2.CAP_PROP_FPS) * period_of_inference
+    fps = vs.get(cv2.CAP_PROP_FPS) / period_of_inference
 
     if vs.isOpened() == False:
         print("Error reading video file")
